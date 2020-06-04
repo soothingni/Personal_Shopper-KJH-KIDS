e73d2f695f9662977ee84f8e62fd65b89e74d0ab
seonggook-jeong <okepower@naver.com>
2020-06-04 16:28:26 +0900

    remove

--- web/ANNsCloset/login/admin.py
+++
@@ -1,6 +0,0 @@
-from .models import Blog
-from django.contrib import admin
-
-
-# Register your models here.
-admin.site.register(Blog)
--- web/ANNsCloset/step1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Step1Config(AppConfig):
-    name = 'step1'
